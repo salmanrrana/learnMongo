2c0053e2d5b5da4f7bbec6380aed2b655234f0dd
Salman Rana <salman.r.rana@gmail.com>
2018-05-12 18:31:57 -0500

    validation tests added, added some requirements for the schema

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe'});
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(() => done());
   });
@@ -49,6 +49,15 @@ describe('Updating records', () => {
       User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
       done
     );
+  });
+
+  it('A user can have their postCount incremented by 1', (done) => {
+    User.update({ name: 'Joe'}, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      })
   });
 
 });
---
+++ users/test/validation_test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Validating records', () => {
+  it('Requires a username', () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name is required.');
+  });
+
+  it('Requires a user\'s name longer than two characters', () => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name must be longer than two characters.')
+  });
+
+});
