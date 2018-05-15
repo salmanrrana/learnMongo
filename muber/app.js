4d9cf0e7c6dc793e147de26a40e535e557f4b19d
Salman Rana <salman.r.rana@gmail.com>
2018-05-15 07:05:33 -0500

    added schema for driver, setting up mongoose with mongodb, and finishing first test to see if we can add a driver to the database

--- muber/app.js
+++ muber/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/muber');
 
 // watch for incoming request of method GET to the route
 // http://localhost:3050/api
 
+app.use(bodyParser.json())
 routes(app);
 
 module.exports = app;
--- muber/controllers/drivers_controller.js
+++ muber/controllers/drivers_controller.js
@@ -1,5 +1,12 @@
+const Driver = require('../models/driver');
+
 module.exports = {
   greeting(req,res) {
     res.send({ 'hi': 'there'});
+  },
+
+  create(req,res) {
+    console.log(req.body);
+    res.send({ hi: 'there' })
   }
 };
